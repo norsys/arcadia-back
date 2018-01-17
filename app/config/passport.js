aaf79a8e7fbb463b6debbf5d5c34df9957952493
med amine <maammach@norsys.fr>
2018-01-17 09:42:57 +0000

    add comment to push

--- app/config/passport.js
+++ app/config/passport.js
@@ -12,6 +12,7 @@ const setup = () => {
     passwordField: 'password'
   };
 
+  //comment
   passport.use(new LocalStrategy(options, (email, password, done) => {
     return models.User.findOne({ where: { email: email } })
       .then(user => {
