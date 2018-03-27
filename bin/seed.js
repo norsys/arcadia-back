3f23d2b0e761e79f5ce31b9f24f3a3f075018704
Zakaryae Azrour AMEKKAOUI <zakaryae.amekkaoui@gmail.com>
2018-03-27 16:07:03 +0100

    ajout des images des catégories et la colonne agence_id

--- bin/seed.js
+++ bin/seed.js
@@ -19,45 +19,45 @@ const agencies = [
 ];
 
 const categories = [
-  { id: 1, name: 'CONVIVALITE' },
-  { id: 2, name: 'BUSINESS AGENCE' },
-  { id: 3, name: 'PRATIQUES' },
-  { id: 4, name: 'CONNEXION' },
-  { id: 5, name: 'FONDATION' },
-  { id: 6, name: 'VIE DE GROUPE' },
-  { id: 7, name: 'RH' },
-  { id: 8, name: 'NAF' }
+  { id: 1, name: 'CONVIVALITE' , image: 'planet-1.png', imageZoom: 'planet-1-zoom.png'},
+  { id: 2, name: 'BUSINESS AGENCE' , image: 'planet-2.png', imageZoom: 'planet-2-zoom.png'},
+  { id: 3, name: 'PRATIQUES', image: 'planet-3.png', imageZoom: 'planet-3-zoom.png' },
+  { id: 4, name: 'CONNEXION' , image: 'planet-4.png', imageZoom: 'planet-4-zoom.png'},
+  { id: 5, name: 'FONDATION' , image: 'planet-5.png', imageZoom: 'planet-5-zoom.png'},
+  { id: 6, name: 'VIE DE GROUPE' , image: 'planet-5.png', imageZoom: 'planet-6-zoom.png'},
+  { id: 7, name: 'RH' , image: 'planet-7.png', imageZoom: 'planet-7-zoom.png'},
+  { id: 8, name: 'NAF', image: 'planet-8.png', imageZoom: 'planet-8-zoom.png' }
 ];
 
 const questions = [
-  { id: 1, category_id: 1, question: 'Prends un café avec ton voisin', response: null, inputType: 'CAMERA' },
-  { id: 2, category_id: 1, question: 'Prendre une photo d\'un déjeuner en équipe', response: null, inputType: 'CAMERA' },
-  { id: 3, category_id: 1, question: 'Quel est la passion commune que tu partages avec ton parrain', response: null, inputType: 'TEXT' },
-  { id: 4, category_id: 2, question: 'Selfie avec ton IA', response: null, inputType: 'CAMERA' },
-  { id: 5, category_id: 2, question: 'Cite les 3 projets en cours + ton préféré', response: null, inputType: 'TEXT' },
-  { id: 6, category_id: 2, question: 'Cites 3 plus petits clients de ton agence', response: null, inputType: 'TEXT' },
-  { id: 7, category_id: 3, question: 'Citez les 4 piliers du QP', response: null, inputType: 'TEXT' },
-  { id: 8, category_id: 3, question: 'Nommer la dernière formation UE', response: null, inputType: 'TEXT' },
-  { id: 9, category_id: 3, question: 'Contacter un pirat pour  ?', response: null, inputType: 'TEXT' },
-  { id: 10, category_id: 4, question: 'Créer son compte sur rocket', response: null, inputType: 'TEXT' },
-  { id: 11, category_id: 4, question: 'Télécharger applis mobile norsys', response: null, inputType: 'TEXT' },
-  { id: 12, category_id: 4, question: 'Liker une des pages norsys', response: null, inputType: 'TEXT' },
-  { id: 13, category_id: 5, question: 'Nommer un membre du bureau fondation', response: null, inputType: 'TEXT' },
-  { id: 14, category_id: 5, question: 'Contacter anne pour les actions en cours', response: null, inputType: 'TEXT' },
-  { id: 15, category_id: 5, question: 'Le projet qui te semble le plus interessant', response: null, inputType: 'TEXT' },
-  { id: 16, category_id: 6, question: 'Quel est l\'objet du kit d\'intégration que tu préfères ?', response: null, inputType: 'TEXT' },
-  { id: 17, category_id: 6, question: 'Rébus sur les CRA ?', response: null, inputType: 'TEXT' },
-  { id: 18, category_id: 6, question: 'EN %, combien de personnes ont suivi une formation en 2016 ?', response: null, inputType: 'TEXT' },
-  { id: 19, category_id: 7, question: 'Cites le nom de la RH agence ?', response: null, inputType: 'TEXT' },
-  { id: 20, category_id: 7, question: 'Cites au moins 3 vidéos norsys ?', response: null, inputType: 'TEXT' },
-  { id: 21, category_id: 7, question: 'Cites un dispositif mis en place au sein de norsys pour favoriser l\'équilibre vie pro/vie perso ?', response: null, inputType: 'TEXT' },
-  { id: 22, category_id: 8, question: 'oui ou non ?', response: null, inputType: 'BOOLEAN' },
-  { id: 23, category_id: 8, question: 'plate préférer ?', response: null, inputType: 'TEXT' }
+    { id: 1, category_id: 1, agence_id: 1, question: 'Prends un café avec ton voisin', response: null, inputType: 'CAMERA' },
+    { id: 2, category_id: 1, agence_id: 1, question: 'Prendre une photo d\'un déjeuner en équipe', response: null, inputType: 'CAMERA' },
+    { id: 3, category_id: 1, agence_id: 1, question: 'Quel est la passion commune que tu partages avec ton parrain', response: null, inputType: 'TEXT' },
+    { id: 4, category_id: 2, agence_id: 2, question: 'Selfie avec ton IA', response: null, inputType: 'CAMERA' },
+    { id: 5, category_id: 2, agence_id: 2, question: 'Cite les 3 projets en cours + ton préféré', response: null, inputType: 'TEXT' },
+    { id: 6, category_id: 2, agence_id: 2, question: 'Cites 3 plus petits clients de ton agence', response: null, inputType: 'TEXT' },
+    { id: 7, category_id: 3, agence_id: 3, question: 'Citez les 4 piliers du QP', response: null, inputType: 'TEXT' },
+    { id: 8, category_id: 3, agence_id: 3, question: 'Nommer la dernière formation UE', response: null, inputType: 'TEXT' },
+    { id: 9, category_id: 3, agence_id: 3, question: 'Contacter un pirat pour  ?', response: null, inputType: 'TEXT' },
+    { id: 10, category_id: 4, agence_id: 4, question: 'Créer son compte sur rocket', response: null, inputType: 'TEXT' },
+    { id: 11, category_id: 4, agence_id: 4, question: 'Télécharger applis mobile norsys', response: null, inputType: 'TEXT' },
+    { id: 12, category_id: 4, agence_id: 4, question: 'Liker une des pages norsys', response: null, inputType: 'TEXT' },
+    { id: 13, category_id: 5, agence_id: 5, question: 'Nommer un membre du bureau fondation', response: null, inputType: 'TEXT' },
+    { id: 14, category_id: 5, agence_id: 5, question: 'Contacter anne pour les actions en cours', response: null, inputType: 'TEXT' },
+    { id: 15, category_id: 5, agence_id: 5, question: 'Le projet qui te semble le plus interessant', response: null, inputType: 'TEXT' },
+    { id: 16, category_id: 6, agence_id: 6, question: 'Quel est l\'objet du kit d\'intégration que tu préfères ?', response: null, inputType: 'TEXT' },
+    { id: 17, category_id: 6, agence_id: 6, question: 'Rébus sur les CRA ?', response: null, inputType: 'TEXT' },
+    { id: 18, category_id: 6, agence_id: 6, question: 'EN %, combien de personnes ont suivi une formation en 2016 ?', response: null, inputType: 'TEXT' },
+    { id: 19, category_id: 7, agence_id: 7, question: 'Cites le nom de la RH agence ?', response: null, inputType: 'TEXT' },
+    { id: 20, category_id: 7, agence_id: 7, question: 'Cites au moins 3 vidéos norsys ?', response: null, inputType: 'TEXT' },
+    { id: 21, category_id: 7, agence_id: 7, question: 'Cites un dispositif mis en place au sein de norsys pour favoriser l\'équilibre vie pro/vie perso ?', response: null, inputType: 'TEXT' },
+    { id: 22, category_id: 8, agence_id: 8,question: 'oui ou non ?', response: null, inputType: 'BOOLEAN' },
+    { id: 23, category_id: 8, agence_id: 8,question: 'plate préférer ?', response: null, inputType: 'TEXT' }
 ];
 
 const users = [
   { id: 1, email: 'albator@pirat.fr', password: 'albator',postalCode: '4000',  nickName: 'albator', agence_id: 1, city: 'Lille', avatar: 'avatar-1' },
-  { id: 2, email: 'yaittaleb@pirat.fr', password: 'yaittaleb', postalCode: '4000', nickName: 'yaittaleb', agence_id: 8, city: 'Marrakech', avatar: 'avatar-1' }
+  { id: 2, email: 'yaittaleb@pirat.fr', password: 'yaittaleb', postalCode: '4000', nickName: 'yaittaleb', agence_id: 8, city: 'Marrakech', avatar: 'avatar-1', isAdmin: 1 }
 ];
 
 
